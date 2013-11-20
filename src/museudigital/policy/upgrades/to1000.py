be5e49f9bc5bef5fe9f13a4f0631719c7751d1b8
Gustavo Lepri <gustavolepri@gmail.com>
2013-11-20 10:11:19 -0200

    adicionado novo upgradeSteps para adição dos grupos

--- src/museudigital/policy/upgrades/to1000.py
+++ src/museudigital/policy/upgrades/to1000.py
@@ -18,3 +18,14 @@ def upgrade0to1000(context):
     gtool.addGroup('RevisoresConteudo', title='Revisores de Conteúdo', roles=['Revisor de conteudo'])
     gtool.addGroup('RevisoresConteudo', title='Revisores de Texto', roles=['Revisor de texto'])
     gtool.addGroup('TradutoresTexto', title='Tradutores de texto', roles=['Tradutor de texto'])
+
+
+def upgrade1000to1001(context):
+    """Upgrade from version 0 to version 1000.
+    """
+    gtool = getToolByName(context, 'portal_groups')
+
+    gtool.addGroup('InclusaoImagensLivro', title='Inclusão de Imagens Livro', roles=['Inclusao de imagens livro'])
+    gtool.addGroup('RevisoresConteudoLivro', title='Revisores de Conteúdo Livro', roles=['Revisor de conteudo livro'])
+    gtool.addGroup('RevisoresConteudoLivro', title='Revisores de Texto Livro', roles=['Revisor de texto livro'])
+    gtool.addGroup('TradutoresTextoLivro', title='Tradutores de texto Livro', roles=['Tradutor de texto livro'])
